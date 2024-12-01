5128fd62fef8ef7b4060c4855e03e9eb77b76114
kusa-mochi <whoatemyapplepie@gmail.com>
2024-12-01 13:26:15 +0900

    make base for create a db and tables.

--- src/cmd/db_initializer/main.go
+++ src/cmd/db_initializer/main.go
@@ -20,6 +20,10 @@ const (
 	ColType_TimestampWithTimezone
 )
 
+const (
+	queryLogPrefix string = "*** query ***"
+)
+
 // id 以外の列定義に用いるパラメータを格納するための構造体
 // id はすべてのテーブルで固定のパラメータ値を用いるため、この構造体は用いない。
 type ColAttr struct {
@@ -40,6 +44,66 @@ type DbAttr struct {
 	Tables []TableAttr
 }
 
+func CreateDatabase(db *sql.DB, dbName string) {
+	query := fmt.Sprintf("create database %s", dbName)
+	log.Println(queryLogPrefix, query)
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func CreateTable(db *sql.DB, tableAttr *TableAttr) {
+	query := "create table "
+	query += tableAttr.Name + " "
+	query += "("
+	for idx, col := range tableAttr.Cols {
+		query += col.Name + " "
+		switch col.Type {
+		case ColType_Boolean:
+			query += "boolean"
+		case ColType_CharacterVarying:
+			query += "varchar("
+			query += fmt.Sprintf("%v", col.MaxLength)
+			query += ")"
+		case ColType_Integer:
+			query += "integer"
+		case ColType_Interval:
+			query += "interval"
+		case ColType_TimestampWithTimezone:
+			query += "timestamptz"
+		}
+
+		if col.IsNullable {
+			query += " null"
+		} else {
+			query += " not null"
+		}
+
+		if idx != len(tableAttr.Cols)-1 {
+			query += ", "
+		}
+	}
+	query += ")"
+	log.Println(queryLogPrefix, query)
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 func main() {
 	var (
 		dbPort = flag.Int("db_port", 5432, "port number of the db server")
@@ -47,7 +111,7 @@ func main() {
 			Name: "lite_sns_db",
 			Tables: []TableAttr{
 				{
-					Name: "user",
+					Name: "sns_user",
 					Cols: []ColAttr{
 						{
 							Name:                "name",
@@ -109,21 +173,31 @@ func main() {
 		}
 	)
 
+	// DB名指定無しで接続する。
 	db, err := sql.Open("postgres", fmt.Sprintf("host=lite-sns-db port=%v user=user password=postgres sslmode=disable", *dbPort))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("postgres opened")
+
+	// lite-sns専用DBを作成する。
+	CreateDatabase(db, dbAttr.Name)
+
+	// 一旦接続を切る。
+	db.Close()
+
+	// DB名を指定して接続し直す。
+	db, err = sql.Open("postgres", fmt.Sprintf("host=lite-sns-db port=%v user=user password=postgres dbname=%s sslmode=disable", *dbPort, dbAttr.Name))
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer db.Close()
 
-	log.Println("postgres opened")
+	log.Printf("connected to %s", dbAttr.Name)
 
-	// DBを作成する。
-	stmt, err := db.Prepare(fmt.Sprintf("create database %s", dbAttr.Name))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	_, err = stmt.Exec()
-	if err != nil {
-		log.Fatalln(err)
+	// テーブルを作成する。
+	for _, tableAttr := range dbAttr.Tables {
+		CreateTable(db, &tableAttr)
 	}
 }
