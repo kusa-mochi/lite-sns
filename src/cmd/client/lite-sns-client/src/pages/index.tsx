310d092c70801240bb7ee87529d83db4847dc834
kusa-mochi <whoatemyapplepie@gmail.com>
2024-12-07 14:32:35 +0900

    add a TBD message to the home page.

--- src/cmd/client/lite-sns-client/src/pages/index.tsx
+++ src/cmd/client/lite-sns-client/src/pages/index.tsx
@@ -2,6 +2,9 @@ export default function Home() {
     return (
         <>
             <div>
+                TBD: The promotion page will be here :)
+            </div>
+            <div>
                 Go to <a href="/test">Test Page</a>
             </div>
         </>
