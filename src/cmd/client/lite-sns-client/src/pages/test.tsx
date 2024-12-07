cf348ebcc1af65d0449b5c0cb870222ddddd2fbd
kusa-mochi <whoatemyapplepie@gmail.com>
2024-12-07 14:38:52 +0900

    install @emotion/css and use it to a sample code.

--- src/cmd/client/lite-sns-client/src/pages/test.tsx
+++ src/cmd/client/lite-sns-client/src/pages/test.tsx
@@ -1,7 +1,12 @@
+import { css } from "@emotion/css"
+
 export default function Test() {
+    const testStyle = css`
+        color: blue;
+    `
     return (
         <>
-            <div>This is a test component.</div>
+            <div className={testStyle}>This is a test component.</div>
             <div>
                 <p>Go to <a href="/test2">Test2 Page</a></p>
             </div>
