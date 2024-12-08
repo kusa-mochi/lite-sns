7eb2995e9154f41d47c6f098e4fb76e5bf76412e
kusa-mochi <whoatemyapplepie@gmail.com>
2024-12-08 22:01:35 +0900

    apply a button component for testing.

--- src/cmd/client/lite-sns-client/src/pages/test.tsx
+++ src/cmd/client/lite-sns-client/src/pages/test.tsx
@@ -1,16 +1,14 @@
-import { css } from "@emotion/css"
-import { useTheme } from "../providers/themeProvider"
+import Button from "../components/molecules/button"
 
 export default function Test() {
-    const theme = useTheme()
-    const testStyle = css`
-        color: ${theme.palette.primary.fontColor};
-        background-color: ${theme.palette.primary.main};
-    `
     return (
         <>
-            <div className={testStyle}>This is a test component.</div>
+            <Button primary>
+                This is a test component.
+            </Button>
             <div>
+                <Button>Sign up</Button>
+                <Button>Sign in</Button>
                 <p>Go to <a href="/test2">Test2 Page</a></p>
             </div>
         </>
