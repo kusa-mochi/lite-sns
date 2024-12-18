5d4e6c4ee28a6d15fab0b41c14c68b56f22fc977
kusa-mochi <whoatemyapplepie@gmail.com>
2024-12-18 20:15:27 +0900

    make bases of the signup and signin pages.

--- src/cmd/client/lite-sns-client/src/pages/index.tsx
+++ src/cmd/client/lite-sns-client/src/pages/index.tsx
@@ -1,4 +1,16 @@
+import { css } from "@emotion/css";
+import Button from "../components/molecules/button";
+
 export default function Home() {
+
+    const signupButtonContainerStyle = css`
+        margin: 8px;
+    `
+
+    const signinButtonContainerStyle = css`
+        margin: 8px;
+    `
+
     return (
         <>
             <div>
@@ -6,6 +18,14 @@ export default function Home() {
             </div>
             <div>
                 Go to <a href="/test">Test Page</a>
+            </div>
+            <div>
+                <div className={signupButtonContainerStyle}>
+                    <Button onClick={() => {location.replace("/signup")}}>Sign up</Button>
+                </div>
+                <div className={signinButtonContainerStyle}>
+                    <Button onClick={() => {location.replace("/signin")}}>Sign in</Button>
+                </div>
             </div>
         </>
     )
---
+++ src/cmd/client/lite-sns-client/src/pages/signup.tsx
@@ -0,0 +1,25 @@
+import { MouseEvent, useState } from "react"
+import Button from "../components/molecules/button"
+
+export default function Signup() {
+    const [cnt, setCnt] = useState(0)
+    function ClickTest(e: MouseEvent) {
+        setCnt(cnt + 1)
+        console.log(`x:${e.pageX}, y:${e.pageY}`)
+    }
+    return (
+        <>
+            <Button primary>
+                This is a test component.
+            </Button>
+            <div>
+                <Button enabled={false}>Disabled button</Button>
+                <Button focused>Focused button</Button>
+                <Button active>Active button</Button>
+                <Button onClick={ClickTest}>Click Test</Button>
+                <div>cnt:{cnt}</div>
+                <p>Go to <a href="/test2">Test2 Page</a></p>
+            </div>
+        </>
+    )
+}
