606c72f84d6e744db3169f0ab26b205857051827
kusa-mochi <whoatemyapplepie@gmail.com>
2024-12-28 19:51:37 +0900

    define a provider for client config.

---
+++ src/cmd/client/lite-sns-client/src/providers/configProvider.tsx
@@ -0,0 +1,46 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react"
+
+// app server config
+// NOTE: この値は export せず、useConfig() の戻り値からアクセスして使う想定。
+const configData: LiteSnsClientConfig = {
+    appServer: {
+        apiPrefix: "lite-sns/api/v1/",
+        ip: "localhost",
+        port: 12381,
+    },
+}
+
+type AppServerInfo = {
+    apiPrefix: string
+    ip: string
+    port: number
+}
+
+type LiteSnsClientConfig = {
+    appServer: AppServerInfo
+}
+
+const ConfigContext = createContext<LiteSnsClientConfig | null>(null)
+
+// ConfigContextを使うコンポーネントが呼び出すフック
+export function useConfig(): LiteSnsClientConfig {
+    const theme = useContext(ConfigContext)
+    if (!theme) throw new Error("wrap this component by ConfigProvider")
+    
+    return theme
+}
+
+type ConfigProviderProps = {
+    children: ReactNode
+}
+export const ConfigProvider = (props: ConfigProviderProps) => {
+    const [config, setConfig] = useState<LiteSnsClientConfig | null>(null)
+
+    useEffect(() => {
+        setConfig(configData)
+    }, [])
+
+    if (!config) return <div>Loading...</div>
+
+    return <ConfigContext.Provider value={config}>{props.children}</ConfigContext.Provider>
+}
