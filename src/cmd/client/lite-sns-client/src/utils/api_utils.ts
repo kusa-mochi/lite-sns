19af50f07ebc1037043f4d2ad499299332cb9676
kusa-mochi <whoatemyapplepie@gmail.com>
2025-01-04 22:39:32 +0900

    make an API utll function for calling APIs.

--- src/cmd/client/lite-sns-client/src/utils/api_utils.ts
+++ src/cmd/client/lite-sns-client/src/utils/api_utils.ts
@@ -1,4 +1,4 @@
-export function encodeHTMLForm(data: any) {
+function encodeHTMLForm(data: any) {
     var params = []
     for (const name in data) {
         const val = data[name]
@@ -7,4 +7,28 @@ export function encodeHTMLForm(data: any) {
     }
 
     return params.join("&").replace(/%20/g, "+")
+}
+
+export function callAPI(
+    apiPath: string,
+    method: string,
+    data: any,
+    userId: number,
+    onSuccess: (response: any) => void,
+) {
+    const xmlHttpReq = new XMLHttpRequest()
+    xmlHttpReq.onreadystatechange = function () {
+        const READYSTATE_COMPLETED: number = 4
+        const HTTP_STATUS_OK: number = 200
+        if (
+            this.readyState === READYSTATE_COMPLETED &&
+            this.status === HTTP_STATUS_OK
+        ) {
+            onSuccess(JSON.parse(this.response))
+        }
+    }
+    xmlHttpReq.open(method, apiPath)
+    xmlHttpReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xmlHttpReq.setRequestHeader("X-User-Id", userId.toString())
+    xmlHttpReq.send(encodeHTMLForm(data))
 }
