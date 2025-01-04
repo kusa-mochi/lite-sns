f1f54c3aad71b021927b321e6942f911f8b70950
kusa-mochi <whoatemyapplepie@gmail.com>
2025-01-04 21:25:38 +0900

    make an API handler base.

--- src/cmd/app_server/api_server/get_user_info.go
+++ src/cmd/app_server/api_server/get_user_info.go
@@ -2,10 +2,17 @@ package api_server
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (s *ApiServer) GetUserInfo(c *gin.Context) {
 	log.Println("server GetUserInfo start")
+
+	// TODO
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "GetUserInfo fin",
+	})
 }
