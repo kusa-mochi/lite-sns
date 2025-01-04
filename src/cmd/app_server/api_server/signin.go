1cefc75a32fdbb7b05b9ef147b3a58d7710f774f
kusa-mochi <whoatemyapplepie@gmail.com>
2025-01-04 16:38:13 +0900

    simplify code.

--- src/cmd/app_server/api_server/signin.go
+++ src/cmd/app_server/api_server/signin.go
@@ -53,18 +53,19 @@ func (s *ApiServer) Signin(c *gin.Context) {
 	result := <-resCh
 
 	if result.Error != nil {
-		switch result.Error.Error() {
+		errorMessage := result.Error.Error()
+		switch errorMessage {
 		case "invalid signin data":
 			c.JSON(http.StatusBadRequest, gin.H{
-				"error": result.Error.Error(),
+				"error": errorMessage,
 			})
 		case "internal server error":
 			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": result.Error.Error(),
+				"error": errorMessage,
 			})
 		default:
 			c.JSON(http.StatusServiceUnavailable, gin.H{
-				"error": result.Error.Error(),
+				"error": errorMessage,
 			})
 		}
 		return
