43760467f4363bc7283f569aa2996f9cf0e38fd1
kusa-mochi <whoatemyapplepie@gmail.com>
2025-01-05 20:14:06 +0900

    add a context for authorization.

---
+++ src/cmd/client/lite-sns-client/src/providers/authProvider.tsx
@@ -0,0 +1,43 @@
+import { createContext, Dispatch, ReactNode, useContext, useEffect, useState } from "react"
+
+type Auth = {
+    userId: number
+    tokenString: string
+}
+
+const AuthContext = createContext<Auth | null>(null)
+const SetAuthContext = createContext<Dispatch<Auth | null> | null>(null)
+
+export function useAuth(): Auth {
+    const auth = useContext(AuthContext)
+    if (!auth) throw new Error("wrap this component by AuthProvider")
+    
+    return auth
+}
+
+export function useSetAuth(): Dispatch<Auth | null> {
+    const setAuth = useContext(SetAuthContext)
+    if (!setAuth) throw new Error("wrap this component by AuthProvider")
+    
+    return setAuth
+}
+
+type AuthProviderProps = {
+    children: ReactNode
+}
+export const AuthProvider = (props: AuthProviderProps) => {
+    const [auth, setAuth] = useState<Auth | null>({
+        userId: -1,
+        tokenString: "",
+    })
+
+    if (!auth) return <div>Loading...</div>
+
+    return (
+        <AuthContext.Provider value={auth}>
+            <SetAuthContext.Provider value={setAuth}>
+                {props.children}
+            </SetAuthContext.Provider>
+        </AuthContext.Provider>
+    )
+}
