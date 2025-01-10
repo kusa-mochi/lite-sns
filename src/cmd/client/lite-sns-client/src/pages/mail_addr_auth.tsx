2939be96bf1cca8835dcc3191c11b568c51506c6
kusa-mochi <whoatemyapplepie@gmail.com>
2025-01-11 00:26:23 +0900

    fix page navigations.
    this make it possible to navigate to another page without clear the context values.

--- src/cmd/client/lite-sns-client/src/pages/mail_addr_auth.tsx
+++ src/cmd/client/lite-sns-client/src/pages/mail_addr_auth.tsx
@@ -1,11 +1,13 @@
+import { useNavigate } from "react-router";
 import Button from "../components/molecules/button";
 
 export default function MailAddrAuth() {
+    const navigate = useNavigate()
     return (
         <>
             <div>メールアドレスが認証できました。</div>
             <div>次のボタンからサインインできます。</div>
-            <Button primary onClick={() => location.replace("/signin")}>Signin</Button>
+            <Button primary onClick={() => navigate("/signin")}>Signin</Button>
         </>
     )
 }
