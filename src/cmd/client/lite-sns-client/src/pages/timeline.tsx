ad13bd3fb04424fbb93229a3a7987f206577f275
kusa-mochi <whoatemyapplepie@gmail.com>
2025-03-15 19:16:51 +0900

    make button layout in the timeline items.

--- src/cmd/client/lite-sns-client/src/pages/timeline.tsx
+++ src/cmd/client/lite-sns-client/src/pages/timeline.tsx
@@ -5,6 +5,8 @@ import { useAuth } from "../providers/authProvider";
 import Card from "../components/atoms/card";
 import { useNavigate } from "react-router";
 import UserPage from "../components/templates/user_page";
+import Button from "../components/molecules/button";
+import MenuButton from "../components/molecules/menu_button";
 
 export default function Timeline() {
   const config = useConfig();
@@ -67,14 +69,51 @@ export default function Timeline() {
       <div>{username}&nbsp;としてサインインしています。</div>
       {posts.map((post: any) => {
         return (
+          // <Card key={post.PostId}>
+          //   <div>{post.PostId}</div>
+          //   <div>{post.UserId}</div>
+          //   <div>{post.UserName}</div>
+          //   <div>{post.UserIconBg}</div>
+          //   <div>{post.CreatedAt}</div>
+          //   <div>{post.UpdatedAt}</div>
+          //   <div>{post.PostText}</div>
+          // </Card>
           <Card key={post.PostId}>
-            <div>{post.PostId}</div>
-            <div>{post.UserId}</div>
-            <div>{post.UserName}</div>
-            <div>{post.UserIconBg}</div>
-            <div>{post.CreatedAt}</div>
-            <div>{post.UpdatedAt}</div>
+            <div>
+              {/* 左寄せにするヘッダ要素 */}
+              <div>
+                {/* ユーザーアイコン */}
+                <div></div>
+                {/* ユーザー名 */}
+                <div>{post.UserName}</div>
+                {/* 投稿日時 */}
+                <div>{post.CreatedAt}</div>
+              </div>
+              {/* 右寄せにするヘッダ要素 */}
+              <div>
+                <MenuButton>︙</MenuButton>
+              </div>
+            </div>
+            {/* 投稿本文 */}
             <div>{post.PostText}</div>
+            <div>
+              {/* いいねボタン */}
+              <div>
+                <MenuButton>いいね</MenuButton>
+              </div>
+              {/* お気に入りボタン */}
+              <div>
+                <MenuButton>お気に入り</MenuButton>
+              </div>
+              {/* コメントボタン */}
+              <div>
+                <MenuButton>コメント</MenuButton>
+              </div>
+              {/* シェアボタン */}
+              <div>
+                <MenuButton>シェア</MenuButton>
+              </div>
+            </div>
           </Card>
         );
       })}
