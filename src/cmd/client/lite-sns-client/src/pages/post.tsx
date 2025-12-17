50e1b9061008655c01633cef5b6027ac589d4dc8
kusa-mochi <whoatemyapplepie@gmail.com>
2025-12-17 20:39:28 +0900

    add user name area to the post page.

--- src/cmd/client/lite-sns-client/src/pages/post.tsx
+++ src/cmd/client/lite-sns-client/src/pages/post.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import UserPage from "../components/templates/user_page";
+import { useAuth } from "../providers/authProvider";
+import Card from "../components/atoms/card";
 
 export default function Post() {
+    const auth = useAuth(); // ユーザーIDなどの情報
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        console.log(`tokenString: ${auth.tokenString}`);
+    }, [auth])
     return (
         <UserPage>
-            投稿作成ページ
+            {/* 投稿作成ページ */}
+            <div>{username}&nbsp;としてサインインしています。</div>
+            <Card>
+                
+            </Card>
         </UserPage>
     )
 }
